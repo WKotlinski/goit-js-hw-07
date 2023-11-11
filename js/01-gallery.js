afa0cf692a0500d5349b8c018935b5e3d71a843f
WKotlinski <132941585+WKotlinski@users.noreply.github.com>
2023-11-11 19:04:44 +0100

    Task 1 finished

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,40 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const ul = document.querySelector(".gallery");
 
-console.log(galleryItems);
+galleryItems.map(({ preview, original, description }) => {
+  const li = document.createElement("li");
+  li.innerHTML = `
+  <div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+        <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+        />
+    </a>
+</div>`;
+  ul.insertAdjacentElement("beforeend", li);
+});
+
+const openModal = function (event) {
+  event.preventDefault();
+  const target = event.target;
+  if (!target.classList.contains("gallery__image")) {
+    return;
+  }
+
+  const modalInstance = basicLightbox.create(`
+  <img src="${target.dataset.source}" alt="${target.alt}">
+`);
+
+  modalInstance.show();
+  document.addEventListener("keydown", function (event) {
+    if (event.code === "Escape") {
+      modalInstance.close();
+    }
+  });
+};
+
+ul.addEventListener("click", openModal);
